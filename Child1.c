4968c5163da0bfa7b7ca4254750757b14bf42c5b
674244120-Phisutthisak Srisirilek <674244120@mail.pbru.ac.th>
2025-08-14 11:34:15 +0700

    Add files via upload

---
+++ Child1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	printf("My Bio : \n");
+	printf("ID : 674244120 \n");
+	printf("Name : Phisutthisak Srisirilek \n");
+	printf("____________________________________ \n");
+	getchar();
+return 0;
+}
